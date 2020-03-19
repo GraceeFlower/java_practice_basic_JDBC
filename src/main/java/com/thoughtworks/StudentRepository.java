5f104cfdceb9f433f23676ac91dc74923624753e
GraceeFlower <1491644103@qq.com>
2020-03-19 14:17:16 +0800

    [WM] refactor: seperate the funciton & delete useless code

--- src/main/java/com/thoughtworks/StudentRepository.java
+++ src/main/java/com/thoughtworks/StudentRepository.java
@@ -9,37 +9,40 @@ public class StudentRepository {
   public static final String USER = "root";
   public static final String PASSWORD = "guoer123";
 
-  public void save(List<Student> students) throws ClassNotFoundException, SQLException {
+  public void save(List<Student> students) throws SQLException {
     for (Student student : students) {
       save(student);
     }
   }
 
-  public void save(Student student) throws ClassNotFoundException, SQLException {
-    // TODO:
-//    Class.forName("com.mysql.jdbc.Driver");
+  public void save(Student student) throws SQLException {
     Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
     String sql = "INSERT INTO student_sys VALUES(?, ?, ?, ?, ?, ?)";
     PreparedStatement pre = conn.prepareStatement(sql);
+    prepareStatement(pre, student);
+    pre.execute();
+  }
+
+  private void prepareStatement(PreparedStatement pre, Student student) throws SQLException {
     pre.setString(1, student.getId());
     pre.setString(2, student.getName());
     pre.setString(3, student.getGender());
     pre.setInt(4, student.getAdmissionYear());
     pre.setDate(5, new java.sql.Date(student.getBirthday().getTime()));
     pre.setString(6, student.getClassId());
-
-    pre.execute();
   }
 
-  public List<Student> query() throws ClassNotFoundException, SQLException {
-    // TODO:
+  public List<Student> query() throws SQLException {
     List<Student> stuList = new ArrayList<>();
-    Student stu;
-//    Class.forName("com.mysql.jdbc.Driver");
     Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
     String sql = "SELECT * FROM student_sys";
     Statement stmt = conn.createStatement();
     ResultSet rs = stmt.executeQuery(sql);
+    return getStuList(stuList, rs);
+  }
+
+  private List<Student> getStuList(List<Student> stuList, ResultSet rs) throws SQLException {
+    Student stu;
     while (rs.next()) {
       stu = new Student();
       stu.setId(rs.getString("id"));
@@ -55,48 +58,28 @@ public class StudentRepository {
   }
 
   public List<Student> queryByClassId(String classId) throws SQLException {
-    // TODO:
     List<Student> stuList = new ArrayList<>();
-    Student stu;
     Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
     String sql = "SELECT * FROM student_sys WHERE classId=? ORDER BY id DESC";
     PreparedStatement pre = conn.prepareStatement(sql);
     pre.setString(1, classId);
     ResultSet rs = pre.executeQuery();
-    while (rs.next()) {
-      stu = new Student();
-      stu.setId(rs.getString("id"));
-      stu.setName(rs.getString("name"));
-      stu.setGender(rs.getString("gender"));
-      stu.setAdmissionYear(rs.getInt("admissionYear"));
-      stu.setBirthday(rs.getDate("birthday"));
-      stu.setClassId(rs.getString("classId"));
-
-      stuList.add(stu);
-    }
-    return stuList;
+    return getStuList(stuList, rs);
   }
 
   public void update(String id, Student student) throws SQLException {
-    // TODO:
     Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
     String sql = "UPDATE student_sys set id=?, name=?, gender=?, admissionYear=?, birthday=?, classId=? WHERE id=?";
     PreparedStatement pre = conn.prepareStatement(sql);
-    pre.setString(1, student.getId());
-    pre.setString(2, student.getName());
-    pre.setString(3, student.getGender());
-    pre.setInt(4, student.getAdmissionYear());
-    pre.setDate(5, new java.sql.Date(student.getBirthday().getTime()));
-    pre.setString(6, student.getClassId());
+    prepareStatement(pre, student);
     pre.setString(7, id);
 
     pre.execute();
   }
 
   public void delete(String id) throws SQLException {
-    // TODO:
     Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-    String sql = "DELETE FROM student_sys where id=?";
+    String sql = "DELETE FROM student_sys WHERE id=?";
     PreparedStatement pre = conn.prepareStatement(sql);
     pre.setString(1, id);
 
